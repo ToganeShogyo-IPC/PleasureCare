a01460fc24953a4ae1fb2a7e9dc24ce24122283d
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2024-07-18 16:10:29 +0900

    [add] いろいろ - 問診の一部作成 - ChartJSのNuggetPackageの適用

--- Data/IntakeQuestions.cs
+++ Data/IntakeQuestions.cs
@@ -10,13 +10,15 @@ namespace SmileCare.Data
     public class IntakeQuestions
     {
         public MarkupString question { get; set; }
-        public int score { get; set; }
+        public int score { get; set; } = 0;
+        public bool is_button { get; set; } = false;
+        public MarkupString afterviewStrings { get; set; }
     }
 
-    public class questionsA { 
+    public class questionsA {
         public List<IntakeQuestions> question = new List<IntakeQuestions>()
         {
-            {new IntakeQuestions(){ 
+            {new IntakeQuestions(){
                 question = (MarkupString)"<h2>今日は何年ですか？</h2>",
                 score = 1
             } },
@@ -25,10 +27,12 @@ namespace SmileCare.Data
                 score = 3
             } },
             {new IntakeQuestions(){
-                question = (MarkupString)"<h2>これから表示する三つの言葉を読み上げてください。<h2><h3>後の設問でもう一度聞きますので、覚えてください。</h3><input type=\"button\" class=\"btn btn-primary\" @onclick=\"visible_OnOff(true)\" value=\"表示する\"><h1>桜　猫　電車</h1>",
-                score = 3
+                question = (MarkupString)"<h2>これから表示する三つの言葉を読み上げてください。<h2><h3>後の設問でもう一度聞きますので、覚えてください。</h3>",
+                score = 3,
+                is_button = true,
+                afterviewStrings = (MarkupString)"<h1>桜　猫　電車</h1>"
             } },
-            
+
         };
 
     }

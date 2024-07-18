681041cf41e15b60b066579efcb018012ec8c7fb
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2024-07-18 17:27:44 +0900

    [add] 設問の登録

--- Data/IntakeQuestions.cs
+++ Data/IntakeQuestions.cs
@@ -12,6 +12,7 @@ namespace SmileCare.Data
         public MarkupString question { get; set; }
         public int score { get; set; } = 0;
         public bool is_button { get; set; } = false;
+        public bool is_continue { get; set; } = false;
         public MarkupString afterviewStrings { get; set; }
     }
 
@@ -30,12 +31,36 @@ namespace SmileCare.Data
                 question = (MarkupString)"<h2>これから表示する三つの言葉を読み上げてください。<h2><h3>後の設問でもう一度聞きますので、覚えてください。</h3>",
                 score = 3,
                 is_button = true,
-                afterviewStrings = (MarkupString)"<h1>桜　猫　電車</h1>"
+                afterviewStrings = (MarkupString)"桜　猫　電車"
             } },
-
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>100から7を引くといくつになりますか？</h2>",
+                score = 1,
+            } },
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>93から7を引くといくつになりますか？</h2>",
+                score = 1,
+                is_continue = true,
+            } },
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>86から7を引くといくつになりますか？</h2>",
+                score = 1,
+                is_continue = true,
+            } },
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>これから表示する三つの数字を読み上げてください。<h2>",
+                score = 3,
+                is_button = true,
+                afterviewStrings = (MarkupString)"682"
+            } },
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>3の設問の単語を3つ思い出してもう一度読み上げてください。</h2>",
+                score = 1,
+            } },
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>知っている野菜をできるだけ多く読み上げてください。</h2>",
+                score = 5,
+            } },
         };
-
     }
-
-
 }

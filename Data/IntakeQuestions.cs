2ba8b85a218534e1f245dc0c5d97e71415686012
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2024-07-18 14:58:22 +0900

    [add] てすと

--- Data/IntakeQuestions.cs
+++ Data/IntakeQuestions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,29 @@ namespace SmileCare.Data
 {
     public class IntakeQuestions
     {
+        public MarkupString question { get; set; }
+        public int score { get; set; }
+    }
+
+    public class questionsA { 
+        public List<IntakeQuestions> question = new List<IntakeQuestions>()
+        {
+            {new IntakeQuestions(){ 
+                question = (MarkupString)"<h2>今日は何年ですか？</h2>",
+                score = 1
+            } },
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>今日は何月何日、何曜日ですか？</h2>",
+                score = 3
+            } },
+            {new IntakeQuestions(){
+                question = (MarkupString)"<h2>これから表示する三つの言葉を読み上げてください。<h2><h3>後の設問でもう一度聞きますので、覚えてください。</h3><input type=\"button\" class=\"btn btn-primary\" @onclick=\"visible_OnOff(true)\" value=\"表示する\"><h1>桜　猫　電車</h1>",
+                score = 3
+            } },
+            
+        };
 
     }
+
+
 }

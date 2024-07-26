78f7f304e862c022491fee6a2bca07fdb9525609
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2024-07-26 15:48:56 +0900

    [add] できた

---
+++ Data/APIRef.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PleasureCare.Data
+{
+    public class APIRef
+    {
+        public class DataBody
+        {
+            [JsonProperty("食品名")]
+            public string foodName { get; set; }
+            [JsonProperty("たんぱく質")]
+            public double protain { get; set; }
+            [JsonProperty("食物繊維総量")]
+            public double shokumotsusenni { get; set; }
+            [JsonProperty("ビタミンB6")]
+            public double vitaminBs { get; set; }
+            [JsonProperty("ビタミンB12")]
+            public double vitaminBtwe { get; set; }
+            [JsonProperty("鉄")]
+            public double iron { get; set; }
+            [JsonProperty("亜鉛")]
+            public double aen { get; set; }
+            [JsonProperty("カルシウム")]
+            public double carushium { get; set; }
+            [JsonProperty("食塩相当量")]
+            public double solt { get; set; }
+        }
+
+        public class FoodsAPI
+        {
+            public List<DataBody> DataBody { get; set; }
+            public AccountService.RespInfo RespInfo { get; set; }
+        }
+    }
+}
